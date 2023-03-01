41915890fe0f88f91f5b31f3bbd3e7e07213d432
Nicolas Moraes de Souza <76184413+NicolasMoraesDeSouza@users.noreply.github.com>
2023-02-28 22:46:26 -0300

    Layout pront, apenas faltando adicionar funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,42 @@ const ApiKey = '3a12998b5f071b3069b0754795eb401c'
 
 const App = () => {
   const inputRef = useRef(null)
+  const [ApiData, setApiData] = useState(null)
+
+  const WeatherTypes = [
+    {
+      type: "Clear",
+      img: "https://cdn-icons-png.flaticon.com/512/6974/6974833.png",
+    },
+    {
+      type: "Rain",
+      img: "https://cdn-icons-png.flaticon.com/512/3351/3351979.png",
+    },
+    {
+      type: "Snow",
+      img: "https://cdn-icons-png.flaticon.com/512/642/642102.png",
+    },
+    {
+      type: "Clouds",
+      img: "https://cdn-icons-png.flaticon.com/512/414/414825.png",
+    },
+    {
+      type: "Haze",
+      img: "https://cdn-icons-png.flaticon.com/512/1197/1197102.png",
+    },
+    {
+      type: "Smoke",
+      img: "https://cdn-icons-png.flaticon.com/512/4380/4380458.png",
+    },
+    {
+      type: "Mist",
+      img: "https://cdn-icons-png.flaticon.com/512/4005/4005901.png",
+    },
+    {
+      type: "Drizzle",
+      img: "https://cdn-icons-png.flaticon.com/512/3076/3076129.png",
+    },
+  ];
   
   const fetchWeather = async () => { 
     console.log(inputRef.current.value)
@@ -17,6 +53,11 @@ const App = () => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data, '--data')
+      setApiData(null)
+      setApiData(data)
+    })
+    .then((data) => {
+
     })
     .catch((err) => { console.log(err)})
   
@@ -31,7 +72,9 @@ const App = () => {
 
       <div className='bg-white w-96 p-4 rounded-md'>
         <div className='flex items-center justify-between'>
-
+          <div>
+            
+          </div>
           <input type='text'
           ref={inputRef}
           placeholder='Enter your location...'
@@ -47,6 +90,13 @@ const App = () => {
             />
 
           </button>
+        </div>
+        <div>
+          <div className='text-center flex flex-col gap-6 mt-10'>
+            <p className='text-xl font-semibold'>Mumbai, IN</p>
+            <img src={WeatherTypes[0].img} alt='Weather' className='w-52 mx-auto' />
+            <h3 className='text-2xl font-bold text-zinc-800'> {WeatherTypes[0].type}</h3>
+          </div>
         </div>
       </div>
 
